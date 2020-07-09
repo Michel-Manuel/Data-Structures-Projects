b06115eff77f4769fd3f1e8adb5f194ac57aec9c
Michel-Manuel <67878427+Michel-Manuel@users.noreply.github.com>
2020-07-09 17:30:37 +0000

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,11 @@
+def NumberofWords(a):
+    sentence = a.split()
+    print("The number of words are " ,len(sentence))
+
+
+def main():
+    NumberofWords("The quick brown fox jumps over the lazy dog")
+main()
+    
+
+
